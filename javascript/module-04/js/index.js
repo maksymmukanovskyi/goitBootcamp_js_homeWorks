a6fc71193f087d91b9e3931b7e000cea1a2b0176
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2018-06-30 22:41:21 +0300

    updated HW-4

--- javascript/module-04/js/index.js
+++ javascript/module-04/js/index.js
@@ -8,37 +8,37 @@ const products = {
     cheese: 40,
 };
 
-function Cashier(name = 'name of cashier',productDatabase = 'object'){
+function Cashier(name = 'name of cashier', productDatabase = 'object') {
     this.name = name;
     this.productDatabase = productDatabase;
     this.totalPrice = 0;
     this.customerMoney = 0;
     this.changeAmount = 0;
-    this.greet = function(){
+    this.greet = function () {
         console.log(`Good Afternoon! you served by ${this.name}`);
     };
-    this.onSuccess = function(){
+    this.onSuccess = function () {
         return this.changeAmount > 0 ? console.log(`Thanks for purchase, your change is ${this.changeAmount}`) : console.log(`Thanks for purchase`)
     };
     this.onError = function () {
         console.log(`So Sorry no sufficient amount entered`);
     };
-    this.countTotalPrice = function(order){
-        return this.totalPrice = order.bread * this.productDatabase.bread;
-
+    this.countTotalPrice = function (order) {
+        return this.totalPrice = (order.bread * this.productDatabase.bread) + (order.milk * this.productDatabase.milk) + (order.apples * this.productDatabase.apples) + (order.cheese * this.productDatabase.cheese);
     };
+
     this.getCustomerMoney = function (value) {
         return this.customerMoney = value;
     };
     this.countChange = function () {
-        if(this.customerMoney >= this.totalPrice){
-            return this.changeAmount = this.totalPrice - this.customerMoney;
-        }else{
+        if (this.customerMoney >= this.totalPrice) {
+            return this.changeAmount = this.customerMoney - this.totalPrice;
+        } else {
             return this.changeAmount = null;
         }
     };
     this.reset = function () {
-        return this.totalPrice, this.customerMoney, this.changeAmount = 0;
+        return this.totalPrice = 0, this.customerMoney =0, this.changeAmount = 0;
     };
 
 }
@@ -61,22 +61,22 @@ console.log(anna.changeAmount);
 
 anna.greet();
 anna.countTotalPrice(order);
-// console.log(anna.totalPrice);
-// anna.getCustomerMoney(300);
-// console.log(anna.customerMoney);
-//
-// const result = anna.countChange();
-// console.log(result);
-//
-// if(result !== null){
-//     anna.onSuccess();
-// }else{
-//     anna.onError();
-// }
-//
-// anna.reset();
-//
-// console.log(anna.totalPrice);
-// console.log(anna.customerMoney);
-// console.log(anna.changeAmount);
-//
+console.log(anna.totalPrice);
+anna.getCustomerMoney(300);
+console.log(anna.customerMoney);
+
+const result = anna.countChange();
+console.log(result);
+
+if(result !== null){
+    anna.onSuccess();
+}else{
+    anna.onError();
+}
+
+anna.reset();
+
+console.log(anna.totalPrice);
+console.log(anna.customerMoney);
+console.log(anna.changeAmount);
+
